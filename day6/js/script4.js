03a213c534a26edb55b3af5129e3708bc50e2043
YanaBrewster <yana.brewster@gmail.com>
2019-05-20 20:52:31 +1200

    task 4

--- day6/js/script4.js
+++ day6/js/script4.js
@@ -3,15 +3,25 @@
 // Find the biggest element in an array of numbers
 // Create a script that prompts for a new number and which then gets added to the above array and find the biggest number again.
 
-console.log("JS working");
-var biggestNumber;
-var numberList = [201 , 10, 176, 63, 104]; 
-document.getElementById("numberedList").innerHTML += "<li>" + numberList + "</li>";
-if (numberList [0] > numberList [1]) {
-console.log("0 is greater than 1");
+var biggestNumber= 0;
+var numberList = [201, 10, 176, 63, 104];
+for (var i = 0; i < numberList.length; i++) {
+  if (numberList[i] > biggestNumber) {
+    biggestNumber = numberList[i]
+  }
 }
-//else if{
-//
-// }
-//
-// document.getElementById("num").innerHTML = + biggestNumber + "is biggest number";
+document.getElementById("numberedList").innerHTML += "<p>" + numberList + "</p>";
+document.getElementById("num").innerHTML = + biggestNumber + "is the biggest number";
+
+function addNumber(numberList) {
+  var newNumber = prompt("Add a number");
+for (var i = 0; i < newNumber.length; i++) {
+  if (newNumber[++i] > biggestNumber) {
+    biggestNumber = newNumber[i]
+    document.getElementById("newBiggestNumber").innerHTML = + newNumber + "is not the biggest number";
+  } else {
+    document.getElementById("newBiggestNumber").innerHTML = +  newNumber  + "is the biggest number";
+}
+
+}
+}
