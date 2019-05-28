36ed021f151f53b6cc34c1521fc7468980cfb4c5
YanaBrewster <yana.brewster@gmail.com>
2019-05-28 17:27:49 +1200

    Task 8 complete

--- day6/js/script8.js
+++ day6/js/script8.js
@@ -1,28 +1,39 @@
 // Task 8
 // Create a script that asks for x amount of people. And then asks for their ages and finds the oldest and youngest age. As well as the average age
 
+var ages = [];
 
-var aAgePerson = [];
+for (var i = 0; i < ages.length; i++) {
+    if(ages[i] > oldestAge){
+        oldestAge = ages[i];
+    }
+    if(ages[i] < youngestAge){
+        youngestAge = ages[i];
+    }
+    totalAge = totalAge + ages[i];
+}
 
-  var iAge = parseInt(prompt('How many people?'));
-    for (var i = 0; i < iAge; i++) {
-    var aAgePerson = parseInt(prompt('How old is person ' + (i+1) + '?'));
-    iAge.push(aAgePerson);
+  var numOfPeople = parseInt(prompt('How many people?'));
+    for (var i = 0; i < numOfPeople; i++) {
+    var age = parseInt(prompt('How old is person ' + (i+1) + '?'));
+    ages.push(age);
 }
 
 var oldestAge = 0;
-var youngestAge = 0;
+var youngestAge = [0];
 var totalAge = 0;
-for (var i = 0; i < aAgePerson.length; i++) {
-  if(aAgePerson[i] > oldestAge){
-    oldestAge = aAgePerson[i];
+for (var i = 0; i < ages.length; i++) {
+  if(ages[i] < oldestAge){
+    oldestAge = ages[i];
   }
-  if(aAgePerson[i] > youngestAge){
-    youngestAge = aAgePerson[i];
+  if(ages[i] > youngestAge){
+    youngestAge = ages[i];
   }
-  totalAge = aAgePerson[i];
+  totalAge = totalAge + ages[i];
 }
 
-var avg = totalAge / aAgePerson.length;
+var avg = totalAge / ages.length;
 
+document.getElementById("oldAge").innerHTML += " " + oldestAge + " "
+document.getElementById("youngAge").innerHTML += " " + youngestAge + " "
 document.getElementById("averageAge").innerHTML += "<p>" + avg + "</p>"
