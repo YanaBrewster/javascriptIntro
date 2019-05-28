911970632506b97ce42816e4bc2d5bdb999b12d4
YanaBrewster <yana.brewster@gmail.com>
2019-05-29 10:26:47 +1200

    completed 10 tasks

--- day6/js/script10.js
+++ day6/js/script10.js
@@ -1,2 +1,41 @@
 // Task 10
 // Create a password checker from a prompt which will check to see is a password is strong enough. Min 5 characters, max 20, 1 number -->
+validatePassword();
+
+function validatePassword(){
+var password = prompt("Enter your password");
+var valid = false;
+var numInit = false;
+var dblnum = false;
+
+if (password.length < 5) {
+  console.log("Your password needs to be at least 5 characters");
+}
+else if (password.length > 20) {
+  console.log("Your password needs to be less than 20 characters");
+}
+else {
+  for (var i = 0; i < password.length; i++) {
+   if (numInit === true) {
+     console.log("There is a number");
+     dblnum = true;
+     break;
+   }
+   if (parseInt(password[i])){
+      numInit = true;
+   }
+  }
+  if (numInit === false){
+    console.log("include a number");
+  } else if (dblnum === false) {
+    valid = true;
+  }
+}
+
+if (valid === true){
+  console.log("your password is set");
+} else{
+  validatePassword();
+}
+
+}
--- day6/js/script9.js
+++ day6/js/script9.js
@@ -1,2 +1,29 @@
 // Task 9
 // Create a script which is going to act as a calculator. You should be asked for 3 prompts which will then output the end calculation. It should be able to do all 4 mathematical options
+
+var num1 = parseInt(prompt("What is the first number input?"));
+var num2 = parseInt(prompt("What is the second number input?"));
+
+var maths = prompt("Choose  +, -, /(divide by), *(x)");
+
+console.log(num1);
+console.log(num2);
+console.log(maths);
+
+var total;
+
+if (maths === "+"){
+  total = num1 + num2;
+  document.getElementById("total").innerHTML += " " +num1 + maths + num2+ " = " + total + " "
+} else if (maths === "-"){
+  total = num1 - num2;
+  document.getElementById("total").innerHTML += " " +num1 + maths + num2+ " = " + total + " "
+} else if (maths === "*"){
+  total = num1 * num2;
+  document.getElementById("total").innerHTML += " " +num1 + maths + num2+ " = " + total + " "
+} else if (maths === "/"){
+  total = num1 / num2;
+  document.getElementById("total").innerHTML += " " +num1 + maths + num2+ " = " + total + " "
+} else{
+  document.getElementById("total").innerHTML += "Invalid input, refresh and try again"
+}
