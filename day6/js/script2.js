f22b8e1af201470392ad76182e610eb6c032ba1d
Yana Brewster <270010939@bun2-206-25.local>
2019-05-28 13:54:04 +1200

    Task 8 attempt

--- day6/js/script2.js
+++ day6/js/script2.js
@@ -3,9 +3,9 @@
 // // 2. Create a script which calculates how old someone is based on their birth year
 // Redo task #1 but based on birth year
 
-// var yearBorn = parseInt(prompt("Please enter the year you were born to see if you are old enough to buy alcohol"));
-//   if(yearBorn < 2001 ){
-//   document.getElementById('alcYear'). innerHTML = "<h4>You can buy alcohol!</h4>"
-//   } else {
-//   document.getElementById('alcYear'). innerHTML = "<h4>You can't buy alcohol!</h4>"
-//   }
+var yearBorn = parseInt(prompt("Please enter the year you were born to see if you are old enough to buy alcohol"));
+  if(yearBorn < 2001 ){
+  document.getElementById('alcYear'). innerHTML = "<h4>You can buy alcohol!</h4>"
+  } else {
+  document.getElementById('alcYear'). innerHTML = "<h4>You can't buy alcohol!</h4>"
+  }
--- day6/js/script8.js
+++ day6/js/script8.js
@@ -1,2 +1,28 @@
 // Task 8
 // Create a script that asks for x amount of people. And then asks for their ages and finds the oldest and youngest age. As well as the average age
+
+
+var aAgePerson = [];
+
+  var iAge = parseInt(prompt('How many people?'));
+    for (var i = 0; i < iAge; i++) {
+    var aAgePerson = parseInt(prompt('How old is person ' + (i+1) + '?'));
+    iAge.push(aAgePerson);
+}
+
+var oldestAge = 0;
+var youngestAge = 0;
+var totalAge = 0;
+for (var i = 0; i < aAgePerson.length; i++) {
+  if(aAgePerson[i] > oldestAge){
+    oldestAge = aAgePerson[i];
+  }
+  if(aAgePerson[i] > youngestAge){
+    youngestAge = aAgePerson[i];
+  }
+  totalAge = aAgePerson[i];
+}
+
+var avg = totalAge / aAgePerson.length;
+
+document.getElementById("averageAge").innerHTML += "<p>" + avg + "</p>"
